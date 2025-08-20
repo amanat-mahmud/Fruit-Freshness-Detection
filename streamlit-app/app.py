d4d16cd685786c81d375cd3a33d77f5cc5a84e7f
Amanat Ullah Mahmud <107941342+amanat-mahmud@users.noreply.github.com>
2025-08-20 08:45:20 +0600

    Update app.py

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image 
 from model_helper import predict
 
+st.set_page_config(page_title="🍓 Fruit Freshness Detection", page_icon="🍓", layout="centered")
 st.title("Fruit Freshness Detection")
 
 uploaded_file = st.file_uploader("Upload the file", type=["jpg", "png"])
@@ -10,4 +11,5 @@ if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB") 
     st.image(image, caption="Uploaded File", use_container_width=True)
     prediction = predict(image)                    
+
     st.info(f"Predicted Class: {prediction}")
